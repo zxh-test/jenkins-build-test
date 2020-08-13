987244e4ab37c9d1ae5ed0f3f1ece21087e8191c
xiaohe.zhang <xiaohe.zhang@zentertain.net>
2020-08-13 16:05:42 +0800

    xx

---
+++ mydemo/src/com/jenkinsbuild/TestNGDemo.java
@@ -0,0 +1,14 @@
+package com.jenkinsbuild;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestNGDemo {
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+
+
+
+}
